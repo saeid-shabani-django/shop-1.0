bae6ff0c768893e700be16c2ec7e401d76d4927b
saeid shabani <django_saeid.sh@proton.me>
2024-09-26 20:10:59 +0330

    CREATE custom user,and CustomUserAdmin

---
+++ accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import UserChangeForm, UserCreationForm, CustomUserCreationForm, CustomUserChangeForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email','username', )
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
